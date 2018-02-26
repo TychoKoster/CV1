f60de511e1d13cecedfa4f4086209acafb3957f4
Tychokoster <tychokoster@hotmail.com>
2018-02-26 17:52:02 +0100

    some adjustments

--- Lab2/Part-1/denoise.m
+++ Lab2/Part-1/denoise.m
@@ -1,12 +1,11 @@
 function [ imOut ] = denoise( image, kernel_type, varargin)
-
+vars = cell2mat(varargin);
 switch kernel_type
     case 'box'
-        imOut = imboxfilt(image);
+        imOut = imboxfilt(image, vars(1));
     case 'median'
-        imOut= medfilt2(image);
+        imOut= medfilt2(image, [vars(1), vars(1)]);
     case 'gaussian'
-        vars = cell2mat(varargin);
         imOut = imfilter(image, gauss2D(vars(1) , vars(2)));
 end
 end
