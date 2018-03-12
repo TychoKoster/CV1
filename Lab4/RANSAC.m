1480ff0eb30f9159fc0899fad32361cb5553b34e
Tychokoster <tychokoster@hotmail.com>
2018-03-13 00:09:13 +0100

    maketform and imtransform implemented

--- Lab4/RANSAC.m
+++ Lab4/RANSAC.m
@@ -1,36 +1,53 @@
 function [transformation_matrix] = RANSAC(matches, P, N, f1, f2, img1, img2)
+% Coördinates of the all the matching keypoints
 xm1 = f1(1, matches(1,:));
 ym1 = f1(2, matches(1,:));
-T_im1 = [xm1;ym1];
 xm2 = f2(1, matches(2,:)) + size(img1, 2);
 ym2 = f2(2, matches(2,:));
+T_im1 = [xm1;ym1];
 T_im2 = [xm2;ym2];
 max_inliers = 0;
 for i = 1:N
     random_subset = datasample(matches, P, 2);
+    % Get coördinates of the matching key points of the random subset
     xa = f1(1,random_subset(1,:));
     xb = f2(1,random_subset(2,:)) + size(img1, 2);
     ya = f1(2,random_subset(1,:));
     yb = f2(2,random_subset(2,:));
     A = [];
+    % Create A matrix
     for j = 1:size(xa,2)
         A_1 = [xa(j), ya(j), 0, 0, 1, 0; 0, 0, xa(j), ya(j), 0, 1];
         A = [A; A_1];
     end
-%     A = [xa, ya, 0, 0, 1, 0; 0, 0, xa, ya, 0, 1];
     b = [xb; yb];
+    % Calculate transformation parameters
     x = pinv(A)*b(:);
     M = [x(1), x(2); x(3), x(4)];
     t = [x(5); x(6)];
+    % Calculate transformated coördinates
     trans_cor = M * T_im1 + t;
+    % Check which points are in a 10 pixels radius from their orignal point
     inliers = abs(trans_cor - T_im2) < 10;
+    % Count inliers
     count_inliers = sum(inliers(1,:) == 1 & inliers(2,:) == 1);
+    % Replace transformation matrix with the highest inliers
     if max_inliers < count_inliers
         max_inliers = count_inliers;
         transformation_matrix = x;
+        trans_m = M;
+        trans_t = t;
     end
 end
-% tform = maketform('affine', img1, img2);
-% imtransform(img1, tform)
+% Own way
 
+% Matlab way
+tform = maketform('affine', [trans_m; trans_t']);
+
+% tform = maketform('affine', [transformation_matrix(1), transformation_matrix(2); transformation_matrix(3), transformation_matrix(4); transformation_matrix(5), transformation_matrix(6)]);
+new_image = imtransform(img2, tform);
+figure;
+imshow(new_image)
+figure;
+imshow(img1)
 end
