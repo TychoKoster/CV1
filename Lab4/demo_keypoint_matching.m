190be0f0ae3c06118c32c53a98f487d77d55c8d6
Tychokoster <tychokoster@hotmail.com>
2018-03-16 11:13:30 +0100

    Results and interpolation method implemented

--- Lab4/demo_keypoint_matching.m
+++ Lab4/demo_keypoint_matching.m
@@ -42,20 +42,44 @@ for i = 1:h
         transformed_im2(xy_21(1), xy_21(2)) = img2(i, j);
     end
 end
+% Crop images
+
+transformed_im2(~any(transformed_im2, 2), :) = [];
+transformed_im2(:, ~any(transformed_im2, 1)) = [];
+transformed_im1(~any(transformed_im1, 2), :) = [];
+transformed_im1(:, ~any(transformed_im1, 1)) = [];
+
+for i = 1:size(transformed_im1, 1)
+    for j = 1:size(transformed_im1, 2)-1
+        if transformed_im1(i,j) == 0
+            transformed_im1(i,j) = transformed_im1(i, j+1);
+        end
+    end
+end
+
+for i = 1:size(transformed_im2, 1)
+    for j = 1:size(transformed_im2, 2)-1
+        if transformed_im2(i,j) == 0
+            transformed_im2(i,j) = transformed_im2(i, j+1);
+        end
+    end
+end
+
+
 % Show transformation from image 1 to 2
 figure;
 subplot(2,2,1);
 imshow(img1);
 title('Normal image 1');
 subplot(2,2,2);
-imshow(transformed_im2(700:end, :));
+imshow(transformed_im2);
 title('Image 2 transformed to image 1');
 % Show transformation from image 2 to 1
 subplot(2,2,3);
 imshow(img2);
 title('Normal image 2');
 subplot(2,2,4);
-imshow(transformed_im1(600:end, :));
+imshow(transformed_im1);
 title('Image 1 transformed to image 2');
 suptitle('Own interpolation method');
 
