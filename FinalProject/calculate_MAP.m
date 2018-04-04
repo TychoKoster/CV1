29259beca31abf2405586a02a93f5602132154e9
Jeroen Terstall <jeroen_t@hotmail.com>
2018-04-04 17:16:21 +0200

    ?

--- FinalProject/calculate_MAP.m
+++
@@ -1,9 +0,0 @@
-function [MAP] = calculate_MAP(predicted)
-    new_sum = 0;
-    for i = 1:size(predicted, 1)
-        if predicted(i) == 1
-            new_sum =+ sum(predicted(1:i))/i;
-        end
-    end
-    MAP = new_sum/size(predicted, 1);
-end
