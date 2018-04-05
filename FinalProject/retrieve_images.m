bb43209555f6982a9a813bcd082b5d5d1afd29f2
Jeroen Terstall <jeroen_t@hotmail.com>
2018-04-05 14:49:04 +0200

    PATHS!

--- FinalProject/retrieve_images.m
+++ FinalProject/retrieve_images.m
@@ -1,10 +1,12 @@
-function [images] = retrieve_images(path_to_dir, length_of_images)
+function [images, image_paths] = retrieve_images(path_to_dir, length_of_images)
+image_paths = {};
 if nargin == 1
     image_files = dir(path_to_dir);
     images = cell(length(image_files), 1);
     for i = 1:length(image_files)
         image_name = strcat(image_files(i).folder, '/', image_files(i).name);
         images{i} = imread(image_name);
+        image_paths{i} = image_name;
     end
 else
     image_files = dir(path_to_dir);
@@ -12,6 +14,7 @@ else
     for i = 1:length_of_images
         image_name = strcat(image_files(i).folder, '/', image_files(i).name);
         images{i} = imread(image_name);
+        image_paths{i} = image_name;
     end
 end
 
